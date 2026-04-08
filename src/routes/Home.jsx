9c1bbcff15053ff3adac6bee083232a37604e9dd
Whitenessly <whitenessly@gmail.com>
2026-04-08 21:13:59 +0700

    ..

--- src/routes/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <div class="h-screen w-screen flex items-center justify-center">
-            <h1>Home</h1>
-        </div>
-    )
-}
-
-export default Home
